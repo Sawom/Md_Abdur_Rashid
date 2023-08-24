752c0b9b4e908d06ed5950c18e3c8d963863eece
Sawom <asawom250@gmail.com>
2023-08-24 12:45:21 +0600

    side bar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar/Sidebar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-6xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+    <div>
+      <BrowserRouter>
+        <Sidebar></Sidebar>
+      </BrowserRouter>
+      
+    
+    
     </div>
   );
 }
