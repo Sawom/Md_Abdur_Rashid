97acae3ef1d654e8d2110795f9a3eb8f419e9ea7
Sawom <asawom250@gmail.com>
2023-08-24 21:36:19 +0600

    all delete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Sidebar from './Components/Sidebar/Sidebar';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Sidebar></Sidebar>
-      </BrowserRouter>
+    <div  >
+        
+        
       
-    
     
     </div>
   );
