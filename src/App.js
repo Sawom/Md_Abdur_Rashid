1f6c210b3fc1a4c7332a4b5785c2ac702dcab1c8
Sawom <asawom250@gmail.com>
2023-08-25 11:35:12 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home/Home';
 
 function App() {
   return (
     <div  >
-        
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route path='/' element={ <Home></Home> } ></Route>
+            <Route path='/home' element={ <Home></Home> } ></Route>
+          </Routes>
+          <Footer></Footer>
+        </BrowserRouter>
         
       
     
