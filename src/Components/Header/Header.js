6a07936fdb1cc68524a7d807ebed5e18852e7e4c
Sawom <asawom250@gmail.com>
2023-08-25 12:21:25 +0600

    header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='bg-base-200 ' >
+            <div className="navbar  container mx-auto">
+                     {/* 1st */}
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        {/* responsive menu part */}
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li  > <Link to='/home' > <span className='linkStyle' >About</span> </Link> </li>
+                            <li> <Link> <span className='linkStyle' >Item 1</span> </Link> </li>
+                        </ul>
+                        </div>
+                        <p className=" normal-case font-bold text-2xl">Md. Abdur Rashid</p>
+                    </div>
+                    {/* 2nd  */}
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            <li><Link to='/home' > <span className='linkStyle' >About</span> </Link></li>
+                            <li><Link> <span className='linkStyle' >Item 1</span> </Link></li>
+                        </ul>
+                    </div>
+            </div>
         </div>
     );
 };
