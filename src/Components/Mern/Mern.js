fdc32ced18beb0f27f81a165a319f93eb251cc0c
Sawom <asawom250@gmail.com>
2023-08-26 20:19:45 +0600

    mern section done

--- src/Components/Mern/Mern.js
+++ src/Components/Mern/Mern.js
@@ -2,6 +2,9 @@ import React from 'react';
 import mern1 from '../../images/mern/arsTravels/mern1.png';
 import mern2 from '../../images/mern/arsTravels/mern2.png';
 import mern3 from '../../images/mern/arsTravels/mern3.png';
+import mern4 from '../../images/mern/arsTravels/mern4.png';
+import mern5 from '../../images/mern/arsTravels/mern5.png';
+import mern6 from '../../images/mern/arsTravels/mern6.png';
 
 const Mern = () => {
     return (
@@ -42,11 +45,6 @@ const Mern = () => {
                             </figure>
                             <div className="card-body ">
                                 <h2 className="card-title">ARS-Travels</h2>
-                                <p className='pStyle' >ARS-Travels is a full stack MERN Application.
-                                    Dynamic router implemented.
-                                    User can book a tour package, cancel or change
-                                    login, logout, confirm password reset , email verification added.
-                                </p>
                                 <div className="card-actions justify-end">
                                     <a href="https://toursite-44305.web.app/"  target="_blank" >
                                         <button className="btn btn-active btn-neutral"> View</button>
@@ -57,13 +55,13 @@ const Mern = () => {
                     </div>
                     {/* 2nd part*/}
                     <div  >
-                        <div className="card  bg-base-100 shadow-xl">
+                        <div className="card  bg-base-100 shadow-2xl">
                             {/* figure er jaygay carousel use korchi */}
                             <figure > 
                                 <div className="carousel w-full">
                                     {/* 1st slide */}
                                     <div id="slide4" className="carousel-item relative w-full">
-                                        <img alt='projectImage' src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+                                        <img alt='projectImage' src={mern4} className="w-full" />
                                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                                         <a href="#slide6" className="btn btn-circle">❮</a> 
                                         <a href="#slide5" className="btn btn-circle">❯</a>
@@ -71,7 +69,7 @@ const Mern = () => {
                                     </div> 
                                     {/* 2nd slide */}
                                     <div id="slide5" className="carousel-item relative w-full">
-                                        <img alt='projectImage' src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                                        <img alt='projectImage' src={mern5} className="w-full" />
                                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                                         <a href="#slide4" className="btn btn-circle">❮</a> 
                                         <a href="#slide6" className="btn btn-circle">❯</a>
@@ -79,7 +77,7 @@ const Mern = () => {
                                     </div> 
                                     {/* 3rd slide */}
                                     <div id="slide6" className="carousel-item relative w-full">
-                                        <img alt='projectImage' src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                                        <img alt='projectImage' src={mern6} className="w-full" />
                                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                                         <a href="#slide5" className="btn btn-circle">❮</a> 
                                         <a href="#slide4" className="btn btn-circle">❯</a>
@@ -88,14 +86,17 @@ const Mern = () => {
                                 </div>
                             </figure>
                             <div className="card-body">
-                                <h2 className="card-title">Shoes!</h2>
-                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <h2 className="card-title">Planet-Shopping</h2>
+                                {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
                                 <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                                    <a href="https://ecommerceapp-f7c88.web.app/"  target="_blank" >
+                                        <button className="btn btn-active btn-neutral"> View</button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    
                 </div> 
         </div>
     );
